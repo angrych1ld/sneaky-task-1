55de5542e405e4a08f730778654b4b36cc88d3a3
Andrius Bendoraitis <abendoraitis300@gmail.com>
2021-01-14 20:11:05 +0200

    Made the thing

---
+++ DoerExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DoerLib;
+
+namespace DoerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create doer object
+            Doer<int> doer = new Doer<int>();
+
+            // Populate the doer with rules
+            doer
+                .On(i => i % 3 == 0 && i % 5 == 0, i => Console.WriteLine("SneakyBox"))
+                .On(i => i % 3 == 0, i => Console.WriteLine("Sneaky"))
+                .On(i => i % 5 == 0, i => Console.WriteLine("Box"))
+                .OnElse(i => Console.WriteLine(i));
+
+            // Have some data set
+            IEnumerable<int> dataSet = Enumerable.Range(1, 100);
+
+            // Run the doer
+            doer.Do(dataSet);
+        }
+    }
+}
---
+++ DoerLib/Doer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoerLib
+{
+    /// <summary>
+    /// Used to execute actions, based on given conditions.
+    /// </summary>
+    /// <typeparam name="T">Type of element that is being used for this doer</typeparam>
+    public class Doer<T>
+    {
+        /// <summary>
+        /// Defined rules for actions on conditions
+        /// </summary>
+        private List<ConditionAction<T>> conditionActions = new List<ConditionAction<T>>();
+
+        /// <summary>
+        /// Action for when the given element matches no other defined rules
+        /// </summary>
+        private Action<T> wildcard;
+
+        /// <summary>
+        /// Adds a new rule for the doer
+        /// </summary>
+        /// <param name="condition">Condition that has to be met for the action to execute</param>
+        /// <param name="action">Action, that is executed, if the condition is met</param>
+        /// <returns></returns>
+        public Doer<T> On(Predicate<T> condition, Action<T> action)
+        {
+            conditionActions.Add(new ConditionAction<T>(condition, action));
+            return this;
+        }
+
+        /// <summary>
+        /// Adds an action, which is executed if the element matchs no other defined rules
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public Doer<T> OnElse(Action<T> action)
+        {
+            wildcard = action;
+            return this;
+        }
+
+        public void Do(IEnumerable<T> items)
+        {
+            // Iterate through all the items
+            foreach (T item in items)
+            {
+                bool found = false;
+
+                // Check all the conditions
+                foreach (ConditionAction<T> conAc in conditionActions)
+                {
+                    // If item meets a condition, execute the action for that condition
+                    if (conAc.Condition(item))
+                    {
+                        conAc.Action(item);
+                        found = true;
+                    }
+                }
+
+                // If we found no other rules, lets check if we have a wildcard
+                if (!found && wildcard != null)
+                {
+                    wildcard(item);
+                }
+            }
+        }
+
+        private class ConditionAction<Tn>
+        {
+            /// <summary>
+            /// Condition, which has to be met for the action to be executed
+            /// </summary>
+            public Predicate<Tn> Condition { get; private set; }
+
+            /// <summary>
+            /// Action which is executed if the condition is met
+            /// </summary>
+            public Action<Tn> Action { get; private set; }
+
+            public ConditionAction(Predicate<Tn> condition, Action<Tn> action)
+            {
+                Condition = condition;
+                Action = action;
+            }
+        }
+    }
+}
